c50a7bb7a10b165d8d32523afbd47cdc5189e4d4
fl3sc0b <38964606+fl3sc0b@users.noreply.github.com>
2021-05-01 19:19:39 +0200

    Added source code

---
+++ getrndelemuniformlydistributedprob.scala
@@ -0,0 +1,9 @@
+  /**
+   * Gets a random element from a ordered collection of elements whose probability is uniformly distributed
+   * @param n Number of elements of the source collection
+   * @return Index of the randomly selected element
+   */
+  def getRndElemUniformlyDistributedProb(n: Int): Int = {
+    val next = scala.util.Random.nextInt((n * n + n) / 2) + 1
+    (Math.ceil(Math.sqrt(8 * next + 1) - 1) / 2).toInt - 1
+ }
